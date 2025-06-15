a7946605a114562e4bc10362355aa0ad7e36009f
Robert Wagner <dwe@dwe.mshome.net>
2025-06-15 09:31:19 +0200

    getter/setter for dino

--- ArkLazyTool/Dino.cs
+++ ArkLazyTool/Dino.cs
@@ -19,8 +19,23 @@ namespace ArkLazyTool
             this.weight = weight;
             this.stamina = stamina;
             this.torpor = torpor;
-
+        }
+        public int GetHp() { return hp; }
+        public int GetDamage() { return damage; }
+        public int GetSpeed() { return speed; }
+        public int GetFood() { return food; }
+        public int GetOx() { return ox; }
+        public int GetWeight() { return weight; }
+        public int GetStamina() { return stamina; }
+        public int GetTorpor() { return torpor; }
+        public void SetHp(int hp) { this.hp = hp; }
+        public void SetDamage(int damage) { this.damage = damage; }
+        public void SetSpeed(int speed) { this.speed = speed; }
+        public void SetFood(int food) { this.food = food; }    
+        public void SetOx(int ox) { this.ox = ox; }
+        public void SetWeight(int weight) { this.weight = weight; }
+        public void SetStamina(int stamina) { this.stamina = stamina; }
+        public void SetTorpor(int torpor) { this.torpor = torpor; }
 
         }
-    }
 }
