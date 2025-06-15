960244635026ef372f61eebd75898a763f40d050
Robert Wagner <dwe@dwe.mshome.net>
2025-06-15 10:52:41 +0200

    fixed createDinoArray and created CalcMaxBabyLevel

--- ArkLazyTool/BabyCalculator.cs
+++ ArkLazyTool/BabyCalculator.cs
@@ -1,14 +1,16 @@
 ﻿using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
 
 namespace ArkLazyTool
 {
     internal class BabyCalculator
     {
         static int dinoCount = HowManyDinos();
-        static Dino[] dinos = new Dino[dinoCount];
-        static int[] maxLevels = new int[8];
-        static String[] statNames = {"hp","damage","speed","food","ox","weight","stamina","topor"};            
-        private const int BABY_START_LEVEL = 1;
+        static Dino[] dinos = new Dino[dinoCount];        
+        static String[] statNames = {"hp","damage","speed","food","ox","weight","stamina","topor"};        
+        static int[] maxLevels = new int[statNames.Length];
+        const int BABY_START_LEVEL = 1;
         static int babyMaxLevel = 0;
 
         
@@ -34,19 +36,49 @@ namespace ArkLazyTool
         static void CreateDinoArray()
         {
             for (int i = 0; i < dinoCount; i++)
-            {
-                for (int j = 0; j < maxLevels.Length; j++) {
+            {   
+                int[] levels = new int[statNames.Length];
+                for (int j = 0; j < statNames.Length; j++) {
+                    
                     Console.WriteLine($"Pls give the Value of {statNames[j]}");
-                    maxLevels[j] = int.Parse(Console.ReadLine() ?? "0");
-                }
-                dinos[i] = new Dino(maxLevels[0], maxLevels[1], maxLevels[2], maxLevels[3], maxLevels[4], maxLevels[5], maxLevels[6], maxLevels[7]);
+                    levels[j] = int.Parse(Console.ReadLine() ?? "0");
 
+                }
+                dinos[i] = new Dino(levels);
             }
 
         }
 
+        static int CalcMaxBabyLevel()
+        {
+            for (int i = 0; i < dinos.Length; i++)
+            {
+                Dino dino = dinos[i];
+                for (int j = 0; j < statNames.Length; j++)
+                {
+                    string statName = statNames[j];
+                    
+                    // Holt per Reflection die PropertyInfo für das aktuelle Stat (z.B. "hp") aus der Dino-Klasse.
+                    // Anschließend wird der Wert dieser Eigenschaft für das aktuelle Dino-Objekt ausgelesen
+                    // und in einen int umgewandelt.
+                    PropertyInfo property = dino.GetType().GetProperty(statName);
+                    int value = (int)property.GetValue(dino);
+                    
+                    if (maxLevels[j] < value)
+                    {
+                        maxLevels[j] = value;
+                    }
+
+                }
+            }
+            foreach(int i in maxLevels)
+            {
+                babyMaxLevel += maxLevels[i];
+            }
+            return babyMaxLevel;
 
 
+        }
 
     }
 }
