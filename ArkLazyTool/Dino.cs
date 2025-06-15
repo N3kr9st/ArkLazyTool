1769bfc80ea9e44275edfc2af5495571997e2a35
Robert Wagner <dwe@dwe.mshome.net>
2025-06-15 13:13:01 +0200

    Dino's values ​​are now private. No more setters.

--- ArkLazyTool/Dino.cs
+++ ArkLazyTool/Dino.cs
@@ -8,8 +8,9 @@ namespace ArkLazyTool
 {
     internal class Dino
     {   
-        public static String[] statNames = {"Hp","Damage","Speed","Food","Ox","Weight","Stamina","Torpor"};
-        int hp,damage,speed,food,ox,weight,stamina,torpor = 0;
+        public static String[] statNames = {"Hp","Damage","Speed","Food","Ox","Weight",
+                                            "Stamina","Torpor"};
+        private int hp,damage,speed,food,ox,weight,stamina,torpor;
         public Dino(int[] values)
         {
             this.hp = values[0];
@@ -30,14 +31,6 @@ namespace ArkLazyTool
         public int GetWeight() { return weight; }
         public int GetStamina() { return stamina; }
         public int GetTorpor() { return torpor; }
-        public void SetHp(int hp) { this.hp = hp; }
-        public void SetDamage(int damage) { this.damage = damage; }
-        public void SetSpeed(int speed) { this.speed = speed; }
-        public void SetFood(int food) { this.food = food; }    
-        public void SetOx(int ox) { this.ox = ox; }
-        public void SetWeight(int weight) { this.weight = weight; }
-        public void SetStamina(int stamina) { this.stamina = stamina; }
-        public void SetTorpor(int torpor) { this.torpor = torpor; }
-
+ 
         }
 }
