19a4275fd00748091f2b98430faa12bf8e2e8d3a
Robert Wagner <dwe@dwe.mshome.net>
2025-06-15 10:14:56 +0200

    changed name from HowManyBabys to HowManyDinos, CreateDinoArray build

--- ArkLazyTool/BabyCalculator.cs
+++ ArkLazyTool/BabyCalculator.cs
@@ -4,26 +4,48 @@ namespace ArkLazyTool
 {
     internal class BabyCalculator
     {
-        static int dinoCount = HowManyBabys();
+        static int dinoCount = HowManyDinos();
         static Dino[] dinos = new Dino[dinoCount];
+        static int[] maxLevels = new int[8];
+        static String[] statNames = {"hp","damage","speed","food","ox","weight","stamina","topor"};            
         private const int BABY_START_LEVEL = 1;
         static int babyMaxLevel = 0;
+
         
-        public static int HowManyBabys()
+        
+        static int HowManyDinos()
         {
+     
             Console.WriteLine("How many Dinos are used?");
             int dinoCount = int.Parse(Console.ReadLine() ?? "0");
-            if (dinoCount < 1 || dinoCount > 7)
+            if (dinoCount is <= 2 or >= 8)
             {
-                Console.WriteLine("Not a valid input!");
-                return 0;
+                Console.WriteLine("Not a valid input! Try again!");
+                HowManyDinos();
             }
             else
             {
                 Console.WriteLine(dinoCount);
                 return dinoCount;
             }
+            return dinoCount;
         }
+
+        static void CreateDinoArray()
+        {
+            for (int i = 0; i < dinoCount; i++)
+            {
+                for (int j = 0; j < maxLevels.Length; j++) {
+                    Console.WriteLine($"Pls give the Value of {statNames[j]}");
+                    maxLevels[j] = int.Parse(Console.ReadLine() ?? "0");
+                }
+                dinos[i] = new Dino(maxLevels[0], maxLevels[1], maxLevels[2], maxLevels[3], maxLevels[4], maxLevels[5], maxLevels[6], maxLevels[7]);
+
+            }
+
+        }
+
+
 
 
     }
