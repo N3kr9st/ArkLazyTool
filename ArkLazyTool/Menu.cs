6d696652167ff381fcf05d8ba0d685171da0a50b
Robert Wagner <dwe@dwe.mshome.net>
2025-06-15 08:44:45 +0200

    basic menu setup

---
+++ ArkLazyTool/Menu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArkLazyTool
+{
+    internal class Menu
+    {
+        public static void ShowMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to Ark Lazy Tool!");
+            Console.WriteLine("1. Start Game");
+            Console.WriteLine("2. Settings");
+            Console.WriteLine("3. Exit");
+            Console.Write("Please select an option: ");
+            
+            int choice = int.Parse(Console.ReadLine());
+        }
+        
+    }
+}
