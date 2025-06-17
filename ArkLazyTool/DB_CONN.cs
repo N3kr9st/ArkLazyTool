9c5e19c7b3a830ea440d290a1782022770604899
Robert <dwe@dwe.mshome.net>
2025-06-17 10:27:52 +0200

    db_conn first setup try

--- ArkLazyTool/DB_CONN.cs
+++ ArkLazyTool/DB_CONN.cs
@@ -1,8 +1,19 @@
 using System;
+using System.Text;
+using System.Data;
+using System.Data.SQLite;
 
 namespace ArkLazyTool;
 
 public class DB_CONN
 {
+     private SQLiteConnection sqlite;
 
+        public DataClass()
+        {
+              //This part killed me in the beginning.  I was specifying "DataSource"
+              //instead of "Data Source"
+              sqlite = new SQLiteConnection("Data Source=/path/to/file.db");
+
+        }
 }
